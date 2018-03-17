cb20bcde1001a668fc5f4bfadd944528149abd15
Livio Bieri <livio@livio.li>
2018-03-17 13:57:13 +0100

    Added TransactionOverview

--- Screens/TransactionOverviewScreen.js
+++ Screens/TransactionOverviewScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View, TextInput, Button, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, TouchableOpacity, SectionList } from 'react-native';
 import { NavigationActions } from 'react-navigation';
 
 const resetAction = NavigationActions.reset({
@@ -22,29 +22,57 @@ export default class TransactionOverview extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      transactions: [
+    transactions: [
+      {date: 'Today', data: [
+        {to: 'Migros', 'amount': 19.95}]
+      },
+      {date: 'Friday, 16. March 2018', data: [
         {to: 'Migros', 'amount': 19.95},
         {to: 'Coop', 'amount': 29.45},
         {to: 'Migros Restaurant', 'amount': 20.00},
         {to: 'Coop', 'amount': 15.00},
-        {to: 'Migros', 'amount': 98.00},
-      ]
-    };
+        {to: 'Migros', 'amount': 98.00}
+      ]},
+      {date: 'Wednesday, 14. March 2018', data: [
+        {to: 'Migros', 'amount': 19.95},
+        {to: 'Coop', 'amount': 29.45},
+        {to: 'Migros Restaurant', 'amount': 20.00},
+        {to: 'Coop', 'amount': 15.00},
+        {to: 'Migros', 'amount': 98.00}
+      ]}
+    ]};
   }
 
   render() {
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'space-between' }}>
-        <Text>Confirmation</Text>
+      <View style={styles.container}>
+        <SectionList
+          sections={this.state.transactions}
+          renderItem={({item}) => <Text style={styles.item}>{item.to} {item.amount} CHF</Text>}
+          renderSectionHeader={({section}) => <Text style={styles.sectionHeader}>{section.date}</Text>}
+          keyExtractor={(item, index) => index}
+        />
         <View style={{height: 50, width: 400}}>
-          <Button onPress={() => {
-            this.props.navigation.dispatch(resetAction);
+           <Button onPress={() => {
+              this.props.navigation.dispatch(resetAction)
             }}
-            title="Add Payment"
+            title="New Payment"
             color="#DE6517"
           />
         </View>
       </View>
-    );
+    )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+   flex: 1,
+   paddingTop: 22
+  },
+  item: {
+    padding: 10,
+    fontSize: 18,
+    height: 44,
+  },
+})
