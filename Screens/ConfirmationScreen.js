2475a36be5719b7eafdcf55dc06138f207ecd95d
Owen Boyle <owen.boyle61@gmail.com>
2018-03-17 17:33:22 +0100

    Rough draft of ConfirmationScreen

--- Screens/ConfirmationScreen.js
+++ Screens/ConfirmationScreen.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import { StyleSheet, Text, View, TextInput, Button, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
-export default class TransactionOverview extends React.Component {
+export default class ConfirmationScreen extends React.Component {
 
   static navigationOptions = ({ navigation }) => ({
+    transactionValue: 50,
+    taxRate: 0.05,
     title: 'Confirmation',
     headerTitleStyle : {textAlign: 'center',alignSelf:'center', color: 'white'},
     headerTintColor: 'white',
@@ -12,10 +14,26 @@ export default class TransactionOverview extends React.Component {
     },
   });
 
+  const RenderConfirmationRow = (row) => {
+    return (
+      <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
+        <Text style={{fontSize: 30, fontWeight: '200', marginLeft: 10}}>{row.title}</Text>
+        <View style={{marginRight: 10, marginTop: 5, marginBottom: 5}}>
+          <Text style={{fontSize: 30}}>{row.value.toFixed(2)}CHF</Text>
+        </View>
+      </View>
+    )
+  }
+
+
   render() {
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'space-between' }}>
+      <View style={{ flexDirection: 'column', flex: 1, alignItems: 'center', justifyContent: 'space-between' }}>
         <Text>Confirmation</Text>
+        <RenderConfirmationRow row={title:"Merchant Receives", value={transactionValue}}/>
+        <RenderConfirmationRow row={title:"Your Tax", value={transactionValue * taxRate}}/>
+        <RenderConfirmationRow row={title:"Total Debited", value={transactionValue * (1 + taxRate)}}/>
+
         <View style={{height: 50, width: 400}}>
           <Button onPress={() => {
             this.props.navigation.navigate('TransactionOverviewScreen')
